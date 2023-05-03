2b3c5a710082e0af7fb4ae3c5412f2a33a7f62dd
adamsinaga <38689336+adamsinaga@users.noreply.github.com>
2023-05-02 22:01:37 -0400

    Add files via upload

---
+++ cobrancaifmt02/src/main/java/edu/ifmt/cobrancaifmt02/Cobrancaifmt02Application.java
@@ -0,0 +1,13 @@
+package edu.ifmt.cobrancaifmt02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cobrancaifmt02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cobrancaifmt02Application.class, args);
+	}
+
+}
---
+++ cobrancaifmt02/src/test/java/edu/ifmt/cobrancaifmt02/Cobrancaifmt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifmt.cobrancaifmt02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cobrancaifmt02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
